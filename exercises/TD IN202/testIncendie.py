edae0ba76f51a6fd8e3db1b6a35692a4d6fd232d
Clément MAOUCHE <clement.maouche@ens.uvsq.fr>
2021-03-05 17:34:51 +0100

    e"

--- exercises/TD IN202/testIncendie.py
+++ exercises/TD IN202/testIncendie.py
@@ -36,49 +36,58 @@ def bruler(index):
     
 
 def simuler():
-    print(len(casesModif))
+    #print(casesModif)
+    #Changement de couleur des cases avec duree = 0
     for i in casesModif:
-        if cases[i][1] == "red" and cases[i][2] > 0:
+        cases[i][2] -= 1
+        if cases[i][2] == 0:
+            if cases[i][1] == "red":
+                cases[i][1] = "grey"
+                cases[i][2] = dureeCendre
+                c.itemconfig(cases[i][0],fill="grey")
+                #print(i,"devient gris")
+            else:
+                cases[i][1] = "black"
+                c.itemconfig(cases[i][0],fill="black")
+                casesNoires.append(i)
+        
+        #Propagation du feu
+        if cases[i][1] == "red":
             casesVoisines = [i-1,i+1,i-nbParcellesLargeur,i+nbParcellesLargeur]
             for x in casesVoisines:
-                if x >= 0 and x < nbParcellesLargeur*nbParcellesHauteur:
+                if x >= 0 and x < nbParcellesLargeur*nbParcellesHauteur and x not in nouvellesCasesFeu:
                     if cases[x][1] == "yellow":
                         nouvellesCasesFeu.append(x)
                     if cases[x][1] == "green":
                         if random.random() < 0.1:
                             nouvellesCasesFeu.append(x)
-            cases[i][2] -= 1
-        elif cases[i][1] == "red" and cases[i][2] == 0:
-            cases[i][1] = "grey"
-            cases[i][2] = dureeCendre
-            c.itemconfig(cases[i][0],fill="grey")
-        elif cases[i][1] == "grey" and cases[i][2] > 0:
-            cases[i][2] -= 1
-        elif cases[i][1] == "grey" and cases[i][2] == 0:
-            cases[i][1] = "black"
-            c.itemconfig(cases[i][0],fill="black")
-            casesModif.remove(i)
-        
+    #Les cases noires deviennent inertes
+    for i in casesNoires:
+        casesModif.remove(i)
+        casesNoires.clear()
+
     for i in nouvellesCasesFeu:
         bruler(i)
     nouvellesCasesFeu.clear()
+    #afficheCarre(cases)
 
 
 largeurCanvas = 500
 hauteurCanvas = 500
-nbParcellesLargeur = 1
-nbParcellesHauteur = 1
+nbParcellesLargeur = 50
+nbParcellesHauteur = 50
 largeurParcelle = largeurCanvas/nbParcellesLargeur
 hauteurParcelle = hauteurCanvas/nbParcellesHauteur
 couleurs = ["green","yellow","blue"]
 couleursPerso = [get_color(130,200,80),get_color(230,230,100),get_color(0,200,240)]
-poidsForet = 60
-poidsPrairie = 1000
-poidsEau = 10
+poidsForet = 1
+poidsPrairie = 10000
+poidsEau = 1
 casesModif = []
 nouvellesCasesFeu = []
-dureeFeu = 4
-dureeCendre = 4
+casesNoires = []
+dureeFeu = 1
+dureeCendre = 1
 id = 0
 
 fen = Tk()
@@ -89,12 +98,13 @@ c.bind('<Button-1>',AllumerClic)
 bouton = Button(text="Simuler",command=simuler)
 for n in range(0,nbParcellesLargeur*nbParcellesHauteur):
     i = n // nbParcellesLargeur
-    j = n % nbParcellesLargeur
+    j = n % nbParcellesHauteur
     couleur = random.choices(couleurs,[poidsForet,poidsPrairie,poidsEau])[0]
     duree = 0
-    id = c.create_rectangle(j*hauteurParcelle,i*largeurParcelle,j*hauteurParcelle+hauteurParcelle,i*largeurParcelle+largeurParcelle,fill=couleur,width=0)
-    #c.create_text(j*hauteurParcelle+hauteurParcelle/2,i*largeurParcelle+largeurParcelle/2,text=(id,i,j))
+    id = c.create_rectangle(j*largeurParcelle,i*hauteurParcelle,j*largeurParcelle+largeurParcelle,i*hauteurParcelle+hauteurParcelle,fill=couleur,width=0)
     cases.append([id,couleur,duree,i,j])
+    #c.create_text(j*hauteurParcelle+hauteurParcelle/2,i*largeurParcelle+largeurParcelle/2,text=(id))
+    
 i,j = 0,0
 #print("i,j",j*hauteurParcelle,i*largeurParcelle,j*hauteurParcelle+hauteurParcelle,i*largeurParcelle+largeurParcelle)
 #print(cases)
