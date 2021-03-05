0d774618d4d7e591ff25657bf884bffc68a483bd
Clément MAOUCHE <clement.maouche@ens.uvsq.fr>
2021-03-05 15:22:51 +0100

    modif testincendie

--- exercises/TD IN202/testIncendie.py
+++ exercises/TD IN202/testIncendie.py
@@ -11,30 +11,54 @@ def afficheCarre(carre):
 def modif():
     c.itemconfigure(1,fill="black")
 
+def get_color(r, g, b):
+    """ Retourne une couleur à partir de ses composantes r, g, b entre 0 et 255"""
+    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
 def AllumerClic(event):
     x, y = event.x, event.y
     source = c.find_closest(x,y)[0]
-    bruler(source)
-    print(source)
+    #bruler(source)
+    print("ID:",source)
+    for i in range(0,len(cases)):
+        if cases[i][0] == source:
+            print("Index:",i)
+            bruler(i)
+            return
+   
 
-def bruler(tag):
-    if cases[tag-1][1] != "red":
-        c.itemconfig(tag,fill="red")
-        cases[tag-1][1] = "red"
-    casesModif.append(tag)
+def bruler(index):
+    if cases[index][1] != "red":
+        c.itemconfig(cases[index][0],fill="red")
+        cases[index][1] = "red"
+        cases[index][2] = dureeFeu
+    casesModif.append(index)
     
 
 def simuler():
+    print(len(casesModif))
     for i in casesModif:
-        if cases[i-1][1] == "red":
+        if cases[i][1] == "red" and cases[i][2] > 0:
             casesVoisines = [i-1,i+1,i-nbParcellesLargeur,i+nbParcellesLargeur]
             for x in casesVoisines:
-                print(cases[x-1][1])
-                if cases[x-1][1] == "yellow":
-                    nouvellesCasesFeu.append(x)
-                if cases[x-1][1] == "green":
-                    if random.random() < 0.1:
+                if x >= 0 and x < nbParcellesLargeur*nbParcellesHauteur:
+                    if cases[x][1] == "yellow":
                         nouvellesCasesFeu.append(x)
+                    if cases[x][1] == "green":
+                        if random.random() < 0.1:
+                            nouvellesCasesFeu.append(x)
+            cases[i][2] -= 1
+        elif cases[i][1] == "red" and cases[i][2] == 0:
+            cases[i][1] = "grey"
+            cases[i][2] = dureeCendre
+            c.itemconfig(cases[i][0],fill="grey")
+        elif cases[i][1] == "grey" and cases[i][2] > 0:
+            cases[i][2] -= 1
+        elif cases[i][1] == "grey" and cases[i][2] == 0:
+            cases[i][1] = "black"
+            c.itemconfig(cases[i][0],fill="black")
+            casesModif.remove(i)
+        
     for i in nouvellesCasesFeu:
         bruler(i)
     nouvellesCasesFeu.clear()
@@ -42,18 +66,20 @@ def simuler():
 
 largeurCanvas = 500
 hauteurCanvas = 500
-nbParcellesLargeur = 10
-nbParcellesHauteur = 10
+nbParcellesLargeur = 1
+nbParcellesHauteur = 1
 largeurParcelle = largeurCanvas/nbParcellesLargeur
 hauteurParcelle = hauteurCanvas/nbParcellesHauteur
 couleurs = ["green","yellow","blue"]
-poidsForet = 4
-poidsPrairie = 4
-poidsEau = 2
+couleursPerso = [get_color(130,200,80),get_color(230,230,100),get_color(0,200,240)]
+poidsForet = 60
+poidsPrairie = 1000
+poidsEau = 10
 casesModif = []
 nouvellesCasesFeu = []
 dureeFeu = 4
 dureeCendre = 4
+id = 0
 
 fen = Tk()
 cases = []
@@ -66,14 +92,14 @@ for n in range(0,nbParcellesLargeur*nbParcellesHauteur):
     j = n % nbParcellesLargeur
     couleur = random.choices(couleurs,[poidsForet,poidsPrairie,poidsEau])[0]
     duree = 0
-    c.create_rectangle(j*hauteurParcelle,i*largeurParcelle,j*hauteurParcelle+hauteurParcelle,i*largeurParcelle+largeurParcelle,fill=couleur,width=0)
-    c.create_text(j*hauteurParcelle+hauteurParcelle/2,i*largeurParcelle+largeurParcelle/2,text=(n+1,i,j))
-    cases.append([n+1,couleur,duree,i,j])
+    id = c.create_rectangle(j*hauteurParcelle,i*largeurParcelle,j*hauteurParcelle+hauteurParcelle,i*largeurParcelle+largeurParcelle,fill=couleur,width=0)
+    #c.create_text(j*hauteurParcelle+hauteurParcelle/2,i*largeurParcelle+largeurParcelle/2,text=(id,i,j))
+    cases.append([id,couleur,duree,i,j])
 i,j = 0,0
-print("i,j",j*hauteurParcelle,i*largeurParcelle,j*hauteurParcelle+hauteurParcelle,i*largeurParcelle+largeurParcelle)
-print(cases)
-afficheCarre(cases)
-print(c.find_all())
+#print("i,j",j*hauteurParcelle,i*largeurParcelle,j*hauteurParcelle+hauteurParcelle,i*largeurParcelle+largeurParcelle)
+#print(cases)
+#afficheCarre(cases)
+#print(c.find_all())
 bouton.grid(row=0)
 c.grid(row=1)
 
