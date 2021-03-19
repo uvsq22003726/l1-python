fd6c189a0e68ed2964d6718b210e135b02caf4ef
Clément MAOUCHE <clement.maouche@ens.uvsq.fr>
2021-03-19 09:41:09 +0100

    yth

--- exercises/TD IN202/test.py
+++ exercises/TD IN202/test.py
@@ -1,3 +1,7 @@
-import random
-
-print(random.random())
+def match_size(mot,i,j): #renvoie la valeur du plus grand sous-mot commun,
+                       #dans le mot aux positions position i et j avec j < i
+    k = 0
+    while(mot[i+k]==mot[j+k] and i+k < len(mot)):
+        k+=1
+    return k
+print(match_size("ab00abc",4,0))
